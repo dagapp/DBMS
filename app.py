e970f8d411632bc4b576607ae1a6ae63b2fe4446
dagapp <dnagapitov@gmail.com>
2023-10-16 11:45:40 +0300

    Added db_query function for querying a database.

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+import sqlite3
+
 from flask import Flask
+
+DB_PATH = '../test.s3db'
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return 'Hello world!'
+def db_query(query_text):
+	with sqlite3.connect(DB_PATH) as conn:
+		cur = conn.cursor()
+		cur.execute(query_text)
+		return cur.fetchall()
 
-if __name__ == "__main__":
+@app.route('/')
+def root():
+	return str(db_query('SELECT * FROM t1'))
+
+if __name__ == '__main__':
 	app.run(debug=True)
